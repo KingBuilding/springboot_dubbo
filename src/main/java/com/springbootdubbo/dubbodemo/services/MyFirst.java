5ba622bd618ed71cb22d78cd78929504f551319a
jinjian <250354848@qq.com>
2019-05-28 00:36:51 +0800

    提交文件~

---
+++ src/main/java/com/springbootdubbo/dubbodemo/services/MyFirst.java
@@ -0,0 +1,10 @@
+package com.springbootdubbo.dubbodemo.services;
+
+/**
+ * Author: 金🗡
+ * Date: 2019/5/23 0:35
+ */
+public interface MyFirst {
+
+    String first();
+}
---
+++ src/main/java/com/springbootdubbo/dubbodemo/services/impl/MyFirstImpl.java
@@ -0,0 +1,20 @@
+package com.springbootdubbo.dubbodemo.services.impl;
+
+import com.springbootdubbo.dubbodemo.services.MyFirst;
+import org.apache.dubbo.config.annotation.Service;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+
+/**
+ * Author: 金🗡
+ * Date: 2019/5/23 0:36
+ */
+@Service(version ="1.0.0")
+public class MyFirstImpl implements MyFirst {
+
+    RocketMQTemplate rocketMQTemplate;
+    @Override
+    public String first() {
+
+        return "first";
+    }
+}
